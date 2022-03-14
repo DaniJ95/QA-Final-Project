8c0a7d858e4322421fd3103f61e1e1f2eeeee5d4
DaniJ95 <damonjohnson95@hotmail.co.uk>
2022-03-14 16:50:43 +0000

    domain

--- src/main/java/com/qa/pokemon/rest/TestController.java
+++ src/main/java/com/qa/pokemon/rest/TestController.java
@@ -1,5 +1,15 @@
 package com.qa.pokemon.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+	@GetMapping("/test")
+	public String hello() {
+		return "Hello World";
+
+	}
 
 }
