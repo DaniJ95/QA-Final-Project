b03cb6d8ec0575bc71d1d70be0ddbb16fd006170
DaniJ95 <damonjohnson95@hotmail.co.uk>
2022-03-18 12:52:34 +0000

    Fixed auto increment for ID

--- src/main/java/com/qa/pokemon/domain/Pokemon.java
+++ src/main/java/com/qa/pokemon/domain/Pokemon.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity //Marking this class for SQL
@@ -22,6 +23,7 @@ public class Pokemon {
 	 * special Defence int
 	 */
 @Id
+@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private Long id;
 @GeneratedValue
 	private String name;
